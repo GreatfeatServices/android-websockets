ae54197f7f29e11964278be326e1e4fd761d7c17
Ariel Silva Jr <ceosilvajr@gmail.com>
2017-10-12 20:22:06 +0800

    Initial commit

---
+++ app/src/main/java/com/socket/ceosilvajr/websocket/MainActivity.java
@@ -0,0 +1,90 @@
+package com.socket.ceosilvajr.websocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+    private TextView output;
+    private OkHttpClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.start);
+        output = (TextView) findViewById(R.id.output);
+        client = new OkHttpClient();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
+    }
+
+    private void start() {
+        Request request = new Request.Builder().url("ws://www.oneph.top/cable").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket(request, listener);
+        client.dispatcher().executorService().shutdown();
+    }
+
+    private void output(final String txt) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(output.getText().toString() + "\n\n" + txt);
+            }
+        });
+    }
+
+    private final class EchoWebSocketListener extends WebSocketListener {
+        private static final int NORMAL_CLOSURE_STATUS = 1000;
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            webSocket.send("Hello, it's Uncle bob!");
+            webSocket.send("What's up ?");
+            webSocket.send(ByteString.decodeHex("It works!"));
+            webSocket.close(NORMAL_CLOSURE_STATUS, "Closed!");
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            output("Receiving : " + text);
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            output("Receiving bytes : " + bytes.hex());
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+            output("Closing : " + code + " / " + reason);
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            output("Error : " + t.getMessage());
+            Log.d(MainActivity.this.getClass().getName(), t.getMessage());
+            Log.d(MainActivity.this.getClass().getName(), webSocket.toString());
+            Log.d(MainActivity.this.getClass().getName(), response.toString());
+        }
+    }
+
+}
